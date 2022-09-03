c02bfa3f1968f7981a513ff31f77be325274e359
Yotsura <gxb65358@gmail.com>
2022-09-04 05:00:53 +0900

    モバイル表示の際、メニューがはみ出るの修正

--- src/components/category/CategoryCtrl.vue
+++ src/components/category/CategoryCtrl.vue
@@ -17,7 +17,7 @@ const selectedWorkMenu = ref(1);
       :isChecked="selectedWorkMenu == 2" >-AddCategory</label>
     <input type="radio" id="option2-3" :value="3" v-model="selectedWorkMenu" class="d-none"/>
     <label for="option2-3" class="menulink px-2 pb-2 flex-fill text-nowrap"
-      :isChecked="selectedWorkMenu == 3" >-AddWork</label>
+      :isChecked="selectedWorkMenu == 3" >-Upload</label>
   </div>
   <transition name="fade" mode="out-in">
     <div v-if="selectedWorkMenu == 1">
