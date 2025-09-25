3b57e0e73d5ff345bb0f419b5b71a4b0e9474e13
Yotsura <gxb65358@gmail.com>
2025-09-25 14:05:11 +0900

    問題の解決：すべてのリクエストがindex.htmlにリダイレクトされているため、JavaScriptファイルへのリクエストもHTMLファイルが返される

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
@@ -17,21 +18,12 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return id
-            .toString()
-            .split('node_modules/')[1]
-            .split('/')[0]
-            .toString();
+            return 'vendor';
           }
         },
-        chunkFileNames: (chunkInfo) => {
-          const facadeModuleId = chunkInfo.facadeModuleId
-          ? chunkInfo.facadeModuleId.split('/')
-          : [];
-          const fileName =
-          facadeModuleId[facadeModuleId.length - 2] || '[name]';
-          return `js/${fileName}/[name].[hash].js`;
-        }
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
       }
     }
   }
