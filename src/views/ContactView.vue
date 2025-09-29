39188223d8c4463b3d3c55eb5d326aca359a6645
Yotsura <gxb65358@gmail.com>
2025-09-29 21:12:34 +0900

    contactにも各リンクを追加

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import NavBarVue from '@/components/NavBar.vue';
 import PostVue from '@/components/post/postForm.vue'
+import LinkBarVue from "@/components/LinkBar.vue";
 
 </script>
 
 <template>
   <NavBarVue />
+  <LinkBarVue />
   <PostVue />
 </template>
 
